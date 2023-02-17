80be1d07f0d82e6d48499bc85cb9828ba5a53f93
siddarth019 <siddarthsingh789@gmail.com>
2023-02-17 15:55:08 +0530

    first api

---
+++ app_api/urls.py
@@ -0,0 +1,11 @@
+from .views import RegisterAPI , LoginAPI , ChangePasswordView
+from knox import views as knox_views
+from django.urls import path , include
+
+urlpatterns = [
+    path('api/register/', RegisterAPI.as_view(), name='register'),
+    path('api/login/', LoginAPI.as_view(), name='login'),
+    path('api/logout/', knox_views.LogoutView.as_view(), name='logout'),
+    path('api/change-password/', ChangePasswordView.as_view(), name='change-password'),
+    path('api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
+]
